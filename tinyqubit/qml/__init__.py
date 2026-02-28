220152b5dcfbd1b392466b45a7b626f68da086c2
jonirajala <joni.m.rajala@gmail.com>
2026-02-28 17:52:34 +0200

    renaming

--- tinyqubit/qml/__init__.py
+++ tinyqubit/qml/__init__.py
@@ -1,6 +1,6 @@
 from .feature_map import angle_feature_map, basis_feature_map, amplitude_feature_map, zz_feature_map, pauli_feature_map
 from .ansatz import strongly_entangling_layers, basic_entangler_layers
-from .optimize import GradientDescent, Adam, SPSA, QNG
+from .optimizers import GradientDescent, Adam, SPSA, QNG
 from .kernel import quantum_kernel, kernel_matrix
 from .cost import predict, cross_entropy_cost, mse_cost, fidelity_cost
 from .hamiltonian import maxcut_hamiltonian
